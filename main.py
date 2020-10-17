4d95adaca07ccf04982d85fbeb6289c88e8f7be6
kaneda <samflynn19121@gmail.com>
2020-10-17 21:17:28 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,64 @@
+import torch
+import torch.nn as nn
+import torchvision
+import torchvision.transforms as transforms
+import torch.optim as optim
+import matplotlib.pyplot as plt
+from tqdm.autonotebook import tqdm
+from models import *
+
+device = torch.device('cuda')
+print('Using device:', device)
+print('GPU:', torch.cuda.get_device_name(0))
+net = CNN().to(device)
+
+transform = transforms.Compose(
+    [transforms.ToTensor(),
+     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))]
+)
+
+trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform)
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=4, shuffle=True)
+
+testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform)
+testloader = torch.utils.data.DataLoader(testset, batch_size=4, shuffle=True)
+
+classes = ('plane', 'car', 'bird', 'cat', 'deer', 
+           'dog', 'frog', 'horse', 'ship', 'truck')
+
+
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+
+num_epochs = 15
+losses = []
+batches = len(trainloader)
+
+def train():
+	print("TRAINING")
+	for epoch in range(num_epochs):
+
+		progress = tqdm(enumerate(trainloader), desc="Loss: ", total=batches)
+
+		total_loss = 0
+		for i, (inputs, labels) in progress:
+			
+			inputs, labels = inputs.to(device), labels.to(device)
+
+			optimizer.zero_grad()
+
+			output= net(inputs)
+			loss = criterion(output, labels)
+			
+			loss.backward()
+			optimizer.step()
+			
+			current_loss = loss.item()
+			total_loss += current_loss
+
+			progress.set_description("Loss: {:.4f}".format(total_loss/(i+1)))
+
+		losses.append(total_loss/batches)
+		print(f"Epoch {epoch+1}/{num_epochs}, Loss: {total_loss/batches}")
+
+
