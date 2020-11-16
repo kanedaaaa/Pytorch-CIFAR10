f07ef76acb4f9cdb7220ef02de3e98f614a80558
kanedaaaa <samflynn19121@gmail.com>
2020-11-16 16:12:10 +0000

    edit googlenet

--- models/googlenet.py
+++ models/googlenet.py
@@ -1,94 +1,96 @@
 import torch
 import torch.nn as nn
 
-class Inception(nn.Module):
-	def __init__(self, inputs, n1x1, n3x3red, n3x3, n5x5red, n5x5, pool):
+class Inception:
+	def __init__(self, in_planes, n1, n3_reduce, n3, n5_reduce, n5, pool_proj):
 		super(Inception, self).__init__()
 
-		self.conv1 = nn.Sequential(
-				nn.Conv2d(inputs, n1x1, kernel_size=1),
-				nn.BatchNorm2d(n1x1),
+		self.block1 = nn.Sequential(
+				nn.Conv2d(in_planes, n1, kernel_size=1),
+				nn.BatchNorm2d(n1),
 				nn.ReLU(True),
 			)
 
-		self.conv2 = nn.Sequential(
-				nn.Conv2d(inputs, n3x3red, kernel_size=1),
-				nn.BatchNorm2d(n3x3red),
+		self.block2 = nn.Sequential(
+				nn.Conv2d(in_planes, n3_reduce, kernel_size=1),
+				nn.BatchNorm2d(n3_reduce),
 				nn.ReLU(True),
-				nn.Conv2d(n3x3red, n3x3, kernel_size=3, padding=1),
-				nn.BatchNorm2d(n3x3),
+
+				nn.Conv2d(n3_reduce, n3, kernel_size=3, stride=1),
+				nn.BatchNorm2d(n3),
 				nn.ReLU(True),
 			)
 
-		self.conv3 = nn.Sequential(
-				nn.Conv2d(inputs, n5x5red, kernel_size=1),
-				nn.BatchNorm2d(n5x5red),
+		self.block3 = nn.Sequential(
+				nn.Conv2d(in_planes, n5_reduce, kernel_size=1),
+				nn.BatchNorm2d(n5_reduce),
 				nn.ReLU(True),
-				nn.Conv2d(n5x5red, n5x5, kernel_size=3, padding=1),
-				nn.BatchNorm2d(n5x5),
+
+				nn.Conv2d(n5_reduce, n5, kernel_size=3, stride=1),
+				nn.BatchNorm2d(n5)
 				nn.ReLU(True),
-				nn.Conv2d(n5x5, n5x5, kernel_size=3, padding=1),
-				nn.BatchNorm2d(n5x5),
+
+				nn.Conv2d(n5, n5, kernel_size=3, stide=1),
+				nn.BatchNorm2d(n5),
 				nn.ReLU(True),
 			)
 
-		self.conv4 = nn.Sequential(
+		self.block4 = nn.Sequential(
 				nn.MaxPool2d(3, stride=1, padding=1),
-				nn.Conv2d(inputs, pool, kernel_size=1),
-				nn.BatchNorm2d(pool),
+				nn.Conv2d(in_planes, pool_proj, kernel_size=1),
+				nn.BatchNorm2d(pool_proj),
 				nn.ReLU(True),
 			)
 
 	def forward(self, x):
-		y1 = self.conv1(x)
-		y2 = self.conv2(x)
-		y3 = self.conv3(x)
-		y4 = self.conv4(x)
-		return torch.cat([y1, y2, y3, y4], 1)
+		y1 = self.block1(x)
+		y2 = self.block2(x)
+		y3 = self.block3(x)
+		y4 = self.block4(x)
 
-class GoogLeNet(nn.Module):
+		return torch.cat([y1,y2,y3,y4], 1)
+
+
+
+class GoogLeNet:
 	def __init__(self):
 		super(GoogLeNet, self).__init__()
+
 		self.pre_layers = nn.Sequential(
 				nn.Conv2d(3, 192, kernel_size=3, padding=1),
 				nn.BatchNorm2d(192),
 				nn.ReLU(True),
 			)
+		self.a3 = Inception(192,  64,  96, 128, 16, 32, 32)
+		  self.b3 = Inception(256, 128, 128, 192, 32, 96, 64)
 
-		self.a3 = Inception(192, 64, 96, 128, 16, 32, 32)
-		self.b3 = Inception(256, 128, 128, 192, 32, 96, 64)
+		  self.maxpool = nn.MaxPool2d(3, stride=2, padding=1)
 
-		self.maxpool = nn.MaxPool2d(3, stride=2, padding=1)
+		  self.a4 = Inception(480, 192,  96, 208, 16,  48,  64)
+		  self.b4 = Inception(512, 160, 112, 224, 24,  64,  64)
+		  self.c4 = Inception(512, 128, 128, 256, 24,  64,  64)
+		  self.d4 = Inception(512, 112, 144, 288, 32,  64,  64)
+		  self.e4 = Inception(528, 256, 160, 320, 32, 128, 128)
 
-		self.a4 = Inception(480, 192, 96, 208, 16, 48, 64)
-		self.b4 = Inception(512, 160, 12, 224, 24, 64, 64)
-		self.c4 = Inception(512, 128, 128, 256, 24, 64, 64)
-		self.d4 = Inception(512, 112, 144, 288, 32, 64, 64)
-		self.e4 = Inception(528, 256, 160, 320, 32, 128, 128)
-		#pool
-		self.a5 = Inception(832, 256, 160, 320, 32, 128, 128)
-		self.b5 = Inception(832, 384, 192, 384, 48, 128, 128)
+		  self.a5 = Inception(832, 256, 160, 320, 32, 128, 128)
+		  self.b5 = Inception(832, 384, 192, 384, 48, 128, 128)
 
+		  self.avg_pool = nn.AvgPool2d(8, stride=1)
+		  self.linear = nn.Linear(1024, 10)
 
-		self.avgpool = nn.AvgPool2d(8, stride=1)
-		self.linear = nn.Linear(1024, 10)
-
-	def forward(self, x):
-		out = self.pre_layers(x)
-		out = self.a3(out)
-		out = self.b3(out)
-		out = self.maxpool(out)
-		out = self.a4(out)
-		out = self.b4(out)
-		out = self.c4(out)
-		out = self.d4(out)
-		out = self.e4(out)
-		out = self.maxpool(out)
-		out = self.a5(out)
-		out = self.b5(out)
-
-		out = self.avgpool(x)
-		out = out.view(out.size(0), -1)
-		out = self.linear(out)
-
-		return out
+		  def forward(self, x):
+			out = self.pre_layers(x)
+			out = self.a3(out)
+			out = self.b3(out)
+			out = self.maxpool(out)
+			out = self.a4(out)
+			out = self.b4(out)
+			out = self.c4(out)
+			out = self.d4(out)
+			out = self.e4(out)
+			out = self.maxpool(out)
+			out = self.a5(out)
+			out = self.b5(out)
+			out = self.avgpool(out)
+			out = out.view(out.size(0), -1)
+			out = self.
